b67bbc43fcc5e7cd09a672feaf041a0942e9153a
xavier-castro <xavier_castro@icloud.com>
2019-08-06 13:55:28 -0500

    Add SmallText component

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -5,11 +5,14 @@ import { Container } from '../components/Container';
 import { Logo } from '../components/Logo';
 import { InputWithButton } from '../components/TextInput';
 import { ClearButton } from '../components/Buttons';
+import { LastConverted } from '../components/Texts';
 
 const TEMP_BASE_CURRENCY = 'USD';
 const TEMP_QUOTE_CURRENCY = 'GBP';
 const TEMP_BASE_PRICE = '100';
 const TEMP_QUOTE_PRICE = '79.74';
+const TEMP_CONVERSION_RATE = 0.7974;
+const TEMP_CONVERSION_DATE = new Date();
 
 class Home extends React.Component {
   handlePressBaseCurrency = () => {
@@ -45,6 +48,12 @@ class Home extends React.Component {
           buttonText={TEMP_QUOTE_CURRENCY}
           editable={false}
           value={TEMP_QUOTE_PRICE}
+        />
+        <LastConverted
+          base={TEMP_BASE_CURRENCY}
+          quote={TEMP_QUOTE_CURRENCY}
+          date={TEMP_CONVERSION_DATE}
+          conversionRate={TEMP_CONVERSION_RATE}
         />
         <ClearButton text="Reverse Currencies" onPress={this.handleSwapCurrency} />
       </Container>
