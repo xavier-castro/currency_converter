1e0d519d528e092b5fdd3f4169a6ce60a8621058
xavier-castro <xavier_castro@icloud.com>
2019-08-07 05:06:09 -0500

    Change currency rate with saga API

--- app/config/sagas.js
+++ app/config/sagas.js
@@ -1,14 +1,47 @@
-import { takeEvery } from 'redux-saga/effects'; // Allows us to listen to any actions
+/* eslint-disable no-console */
+/* eslint-disable prefer-destructuring */
+import {
+  takeEvery, select, call, put,
+} from 'redux-saga/effects'; // Allows us to listen to any actions
 
 // 1. Swap currency
 // 2. Change base currency
 // 3. Make remote request on initial App load
 
-import { SWAP_CURRENCY, CHANGE_BASE_CURRENCY, GET_INITIAL_CONVERSION } from '../actions/currencies';
+import {
+  SWAP_CURRENCY,
+  CHANGE_BASE_CURRENCY,
+  GET_INITIAL_CONVERSION,
+  CONVERSION_RESULT,
+  CONVERSION_ERROR,
+} from '../actions/currencies';
+
+const getLatestRate = currency => fetch(`https://fixer.handlebarlabs.com/latest?base=${currency}`);
 
 function* fetchLatestConversionRates(action) {
-  console.log('TODO: Update the things', action);
-  yield;
+  // getLatestRate('USD')
+  //   .then(res => res.json())
+  //   .then(res => console.log(res))
+  //   .catch(err => console.log('ERROR:', err));
+  try {
+    let currency = action.currency;
+    if (currency === undefined) {
+      currency = yield select(state => state.currencies.baseCurrency);
+    }
+
+    const response = yield call(getLatestRate, currency); // call returns an object from request
+    const result = yield response.json(); // yield waits for conversion to actually happen
+
+    if (result.error) {
+      // If there was an error
+      yield put({ type: CONVERSION_ERROR, error: result.error });
+    } else {
+      // If successful
+      yield put({ type: CONVERSION_RESULT, result });
+    }
+  } catch (err) {
+    yield put({ type: CONVERSION_ERROR, error: err.message });
+  }
 }
 
 // Generator function (function*)
--- app/reducers/currencies.js
+++ app/reducers/currencies.js
@@ -3,6 +3,9 @@ import {
   SWAP_CURRENCY,
   CHANGE_BASE_CURRENCY,
   CHANGE_QUOTE_CURRENCY,
+  GET_INITIAL_CONVERSION,
+  CONVERSION_RESULT,
+  CONVERSION_ERROR,
 } from '../actions/currencies';
 
 const initialState = {
@@ -10,6 +13,7 @@ const initialState = {
   quoteCurrency: 'GBP',
   amount: 100,
   conversions: {},
+  error: null,
 };
 
 const setConversions = (state, action) => {
@@ -54,7 +58,28 @@ const reducer = (state = initialState, action) => {
         quoteCurrency: action.currency,
         conversions: setConversions(state, action),
       };
-
+    case GET_INITIAL_CONVERSION:
+      return {
+        ...state,
+        conversions: setConversions(state, { currency: state.baseCurrency }),
+      };
+    case CONVERSION_RESULT:
+      return {
+        ...state,
+        baseCurrency: action.result.base,
+        conversions: {
+          ...state.conversions,
+          [action.result.base]: {
+            isFetching: false,
+            ...action.result,
+          },
+        },
+      };
+    case CONVERSION_ERROR:
+      return {
+        ...state,
+        error: action.error,
+      };
     default:
       return state;
   }
